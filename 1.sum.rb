2fc0ad5636c16e69fbfa9c0597d9889bcc7ad30c
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-12-02 12:47:34 +0100

    Follow-up for day 1

--- 1.sum.rb
+++ 1.sum.rb
@@ -1,10 +1,19 @@
-#!/usr/bin/env ruby
 # frozen_string_literal: true
 
-puts(ARGF.reduce(0) do |sum, line|
-  next sum if line.empty?
+SPELLED_DIGITS = { one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7,
+                   eight: 8, nine: 9 }.freeze
+VALID_DIGIT = /\d|#{SPELLED_DIGITS.keys.join('|')}/.freeze
+FIRST_DIGIT = /.*?(#{VALID_DIGIT}).*/.freeze
+LAST_DIGIT = /.*(#{VALID_DIGIT}).*/.freeze
 
-  first_digit = line.match(/.*?(\d).*/)[1]
-  last_digit = line.match(/.*(\d).*/)[1]
-  sum + (first_digit + last_digit).to_i
-end)
+def digit_from_string(str)
+  return str.to_i if str in '0'..'9'
+
+  SPELLED_DIGITS[str.to_sym]
+end
+
+ARGF.reduce(0) do |sum, line|
+  first_digit = line.match(FIRST_DIGIT)[1].then { digit_from_string(_1) }
+  last_digit = line.match(LAST_DIGIT)[1].then { digit_from_string(_1) }
+  sum + first_digit * 10 + last_digit
+end.display
