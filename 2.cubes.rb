51fa1c3da2e01900b26bbff49e02c51210d2dd67
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-12-03 10:40:00 +0100

    Solve day 2 part 2

--- 2.cubes.rb
+++ 2.cubes.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
-CUBES = { 'red' => 12, 'green' => 13, 'blue' => 14 }.freeze
-
-ARGF.reduce(0) do |game_ids_sum, line|
+ARGF.reduce(0) do |power_sum, line|
   game_id, *reaches = line.split(/: |, |; /)
-  impossible_move = reaches.map(&:split).find { |cubes, color| cubes.to_i > CUBES[color] }
-  next game_ids_sum if impossible_move
-
-  game_ids_sum + game_id.split.last.to_i
+  minimal_bag = reaches.each_with_object('red' => 0, 'green' => 0, 'blue' => 0) do |cubes, bag|
+    amount, color = cubes.split
+    bag[color] = [amount.to_i, bag[color]].max
+  end
+  power_sum + minimal_bag.values.reduce(:*)
 end.display
