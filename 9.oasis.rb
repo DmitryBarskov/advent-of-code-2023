f8a5024f43f41a191fa28d294708b51cb510b069
Dmitry Barskov <dmitry.barskov64@gmail.com>
2023-12-09 12:47:03 +0100

    Solve d9p2

--- 9.oasis.rb
+++ 9.oasis.rb
@@ -1,13 +1,9 @@
-_input = "0 3 6 9 12 15
-1 3 6 10 15 21
-10 13 16 21 30 45"
-
 def next_in_sequence(seq)
   return 0 if seq.all?(&:zero?)
 
-  diff_seq = seq.each_cons(2).map { |a, b| b - a }
+  diff_seq = seq.each_cons(2).map { |a, b| a - b }
   diff = next_in_sequence(diff_seq)
-  seq.last + diff
+  seq.first + diff
 end
 
 def parse_sequence(line) = line.split.map(&:to_i)
